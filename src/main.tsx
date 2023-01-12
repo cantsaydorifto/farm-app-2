f4ab4603d40295769ef90bd8beadb6495fc9e64f
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-01-12 20:51:50 +0530

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,11 @@ import { ProductContextProvider } from "./context/product/Provider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<ProductContextProvider>
-			<CartContextProvider>
-				<Router>
-					<App />
-				</Router>
-			</CartContextProvider>
-		</ProductContextProvider>
-	</React.StrictMode>
+	<ProductContextProvider>
+		<CartContextProvider>
+			<Router>
+				<App />
+			</Router>
+		</CartContextProvider>
+	</ProductContextProvider>
 );
