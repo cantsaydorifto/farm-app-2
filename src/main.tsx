b15d32188fba19a835698c1e02828e51db422b58
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-01-12 16:48:49 +0530

    add product,cart providers as parents of <App/>

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { CartContextProvider } from "./context/cart/Provider";
+import { ProductContextProvider } from "./context/product/Provider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<ProductContextProvider>
+			<CartContextProvider>
+				<Router>
+					<App />
+				</Router>
+			</CartContextProvider>
+		</ProductContextProvider>
 	</React.StrictMode>
 );
