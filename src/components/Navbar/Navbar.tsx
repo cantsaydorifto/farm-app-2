c0dde340c2bc8568501581968f9bdaa6ad2d1978
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-01-13 22:23:56 +0530

    add homepage logo link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import Sidebar from "../Sidebar/Sidebar";
 import styles from "./Navbar.module.css";
@@ -9,6 +10,7 @@ export default function Navbar() {
 	const sidebarHandler = (): void => {
 		setsidebar((prev) => !prev);
 	};
+	const navigate = useNavigate();
 	return (
 		<>
 			<AnimatePresence>
@@ -34,12 +36,14 @@ export default function Navbar() {
 						src="https://cdn-icons-png.flaticon.com/512/5107/5107718.png"
 						alt="Sidebar Icon"
 					/>
-					<img
-						className={styles.logo}
-						src="https://cantsaydorifto.github.io/farm-app/static/media/vegetables.9c32fd32a85313497d4b.png"
-						alt="Main Logo for the farmers market"
-					/>
-					<h1 className={styles.heading}>Farmer's Market</h1>
+					<div className={styles.homepageLogo} onClick={()=>navigate('/')}>
+						<img
+							className={styles.logo}
+							src="https://cantsaydorifto.github.io/farm-app/static/media/vegetables.9c32fd32a85313497d4b.png"
+							alt="Main Logo for the farmers market"
+						/>
+						<h1 className={styles.heading}>Farmer's Market</h1>
+					</div>
 				</div>
 				<Cart />
 			</nav>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import styles from "./Sidebar.module.css";
 import { motion } from "framer-motion";
 import SidebarContent from "../SidebarContent/SidebarContent";
+import { useNavigate } from "react-router-dom";
 
-interface SidebarProps {
+type props = {
 	sidebarHandler(): void;
-}
+};
 
-export default function Sidebar({ sidebarHandler }: SidebarProps) {
+export default function Sidebar({ sidebarHandler }: props) {
+	const navigate = useNavigate();
+	const logoClickHandler = () => {
+		navigate("/");
+		sidebarHandler();
+	};
 	return (
 		<>
 			<motion.div
@@ -24,14 +30,17 @@ export default function Sidebar({ sidebarHandler }: SidebarProps) {
 						src="https://cdn-icons-png.flaticon.com/512/5107/5107718.png"
 						alt="Sidebar Icon"
 					/>
-					<img
-						className={styles.logo}
-						src={
-							"https://cantsaydorifto.github.io/farm-app/static/media/vegetables.9c32fd32a85313497d4b.png"
-						}
-						alt="Main Logo for the farmers market"
-					/>
-					<h1 className={styles.heading}>Farmer's Market</h1>
+					<div
+						className={styles.homepageLogo}
+						onClick={logoClickHandler}
+					>
+						<img
+							className={styles.logo}
+							src="https://cantsaydorifto.github.io/farm-app/static/media/vegetables.9c32fd32a85313497d4b.png"
+							alt="Main Logo for the farmers market"
+						/>
+						<h1 className={styles.heading}>Farmer's Market</h1>
+					</div>
 				</div>
 				<SidebarContent sidebarHandler={sidebarHandler} />
 			</motion.div>
