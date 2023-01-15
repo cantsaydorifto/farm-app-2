f19b44db6785203bb1b21ecb351908bf7109da0f
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-01-16 01:41:36 +0530

    add add-to-cart button animation

--- src/components/CountDiv/CountDv.tsx
+++ src/components/CountDiv/CountDv.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import styles from "./CountDiv.module.css";
 
@@ -7,17 +8,29 @@ type props = {
 	removeItemhandler: () => void;
 };
 
-const CountDiv = ({ count, addItemhandler, removeItemhandler }:props) => {
+const CountDiv = ({ count, addItemhandler, removeItemhandler }: props) => {
 	return (
-		<div className={styles.count}>
+		<motion.div
+			className={styles.count}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.2 }}
+		>
 			<div onClick={removeItemhandler} className={styles.removeItem}>
 				-
 			</div>
-			<div className="number">{count}</div>
+			<motion.div
+				className="number"
+				initial={{x:-1}}
+				animate={{ x:0 }}
+				key={count}
+			>
+				{count}
+			</motion.div>
 			<div onClick={addItemhandler} className={styles.addItem}>
 				+
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
